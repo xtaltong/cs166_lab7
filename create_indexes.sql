9348708a7486841a8c7881fc7f2d7e110c9b65ff
Crystal Tong <45619087+xtaltong@users.noreply.github.com>
2021-05-26 03:41:10 -0700

    Update create_indexes.sql

--- create_indexes.sql
+++ create_indexes.sql
@@ -1,1 +1,10 @@
-
+CREATE INDEX part_nyc_on_hand_index ON part_nyc (on_hand);
+CREATE INDEX part_sfo_on_hand_index ON part_sfo (on_hand);
+CREATE INDEX part_nyc_color_index ON part_nyc (color);
+CREATE INDEX part_sfo_color_index ON part_sfo (color);
+CREATE INDEX supplier_id_index ON supplier (supplier_id);
+CREATE INDEX supplier_name_index ON supplier (supplier_name);
+CREATE INDEX part_nyc_supplier_index ON part_nyc (supplier);
+CREATE INDEX part_sfo_supplier_index ON part_sfo (supplier);
+CREATE INDEX part_nyc_part_number_index ON part_nyc (part_number);
+CREATE INDEX part_sfo_part_number_index ON part_sfo (part_number);
